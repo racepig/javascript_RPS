9ca8333e29d633b658d1085507cd7635ada57cbd
racepig <84897520+racepig@users.noreply.github.com>
2024-12-18 15:23:55 +0900

    finish rock paper scissors code

--- javascript.js
+++ javascript.js
@@ -1,9 +1,4 @@
-console.log("Hello World")
-
 function getComputerChoice() {
-    let rock = 1;
-    let paper = 2;
-    let scissors = 3;
     let choice = Math.floor(Math.random() * 3) + 1;
     if (choice == 1){
         return "rock"
@@ -13,5 +8,39 @@ function getComputerChoice() {
         return "scissors"
     }
 }
+function getHumanChoice() {
+    let choice = prompt("Choose rock, paper, or scissors");
+    let lower = choice.toLowerCase();
+    if (lower == "rock" || lower == "paper" || lower == "scissors") {
+        return lower;
+    } else {
+        return getHumanChoice();
+    }
+}
 
-console.log(getComputerChoice())
+function playRound(humanScore, computerScore) {
+    if (humanScore == computerScore) {
+        return "Tie";
+    }
+    if (humanScore == "rock" && computerScore == "scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (humanScore == "paper" && computerScore == "rock") {
+        return "You Win! Paper beats Rock";
+    } else if (humanScore == "scissors" && computerScore == "paper") {
+        return "You Win! Scissors beats Paper";
+    } else {
+        return "You Lose! " + computerScore + " beats " + humanScore;
+    }
+}
+
+function playGame() {
+    let round = 0;
+    while (round < 5) {
+        const humanScore = getHumanChoice();
+        const computerScore = getComputerChoice();
+        console.log(playRound(humanScore, computerScore));
+        round++;
+    }
+}
+
+playGame();
